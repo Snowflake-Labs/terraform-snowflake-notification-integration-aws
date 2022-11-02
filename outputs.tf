be149e70139a93a36541670de1171308ff7e8f7c
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-11-01 19:02:13 -0700

    Add outputs.

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,14 @@ output "notification_integration_sns_role_arn" {
   description = "The role arn the snowflake notification integration will assume."
   value       = aws_iam_role.notification_integration_sns_role.arn
 }
+
+output "notification_integration_external_id" {
+  description = "The external ID or the notification integration."
+  value       = snowflake_notification_integration.error_integration.arn
+}
+
+output "notification_integration_user_arn" {
+  description = "The user ARN of the notification integration."
+  value       = snowflake_notification_integration.error_integration.arn
+}
+
